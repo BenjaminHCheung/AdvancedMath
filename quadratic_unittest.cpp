968688a25c8108a7d10034cdd682fff96f7b9778
Benjamin Cheung <benjaminhcheung@gmail.com>
2018-10-30 01:08:32 -0600

    fixed the unit test of a minor error with vectors, also fixed the test values, programed so the tests would pass

--- quadratic_unittest.cpp
+++ quadratic_unittest.cpp
@@ -4,8 +4,8 @@
 TEST(GivenASetOfValuesIntoTheQuadraticFormulaThatWillProduceANonComplexNumber, WhenUsingTheQuadraticFormula_ReturnAVectorOfPredictedValuesInTheFormOfComplexObjects)
 {
     std::vector<Complex> testSolutions{solve_quadratic_equation(1.0, -3.0, 2.0)};
-    std::string expectedFirstComplexNumber{"2.500000 + i0.000000"};
-    std::string expectedSecondComplexNumber{"1.500000 + i0.000000"};
-    EXPECT_TRUE(expectedFirstComplexNumber == testSolutions[1].build_output_string());
-    EXPECT_TRUE(expectedSecondComplexNumber == testSolutions[2].build_output_string());
+    std::string expectedFirstComplexNumber{"2.000000 + i0.000000"};
+    std::string expectedSecondComplexNumber{"1.000000 + i0.000000"};
+    EXPECT_TRUE(expectedFirstComplexNumber == testSolutions[0].build_output_string());
+    EXPECT_TRUE(expectedSecondComplexNumber == testSolutions[1].build_output_string());
 }
--- quadraticfunctions.cpp
+++ quadraticfunctions.cpp
@@ -3,6 +3,14 @@
 std::vector<Complex> solve_quadratic_equation(double a, double b, double c)
 {
     std::vector<Complex> solutionVector;
+    double squared{2.0};
+    double realPart{-b/(2*a)};
+    double quadraticPartPositve{sqrt(pow(b, squared) - (4.0 * a * c))/(2*a)};
+    double quadraticPartNegative{-sqrt(pow(b, squared) - (4.0 * a * c))/(2*a)};
+    Complex firstComplexNumber(realPart + quadraticPartPositve, 0.0);
+    solutionVector.push_back(firstComplexNumber);
+    Complex secondComplexNumber(realPart + quadraticPartNegative, 0.0);
+    solutionVector.push_back(secondComplexNumber);
     return solutionVector;
 }
 
